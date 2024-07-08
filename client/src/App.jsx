7534938eaca969d0622c7f8bdfba1cbe60ce16eb
Habeeb <ademolawaliyu1@gmail.com>
2024-07-08 18:07:57 +0100

    added login

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,15 +4,28 @@ import {BrowserRouter, Route, Routes, Navigate, Outlet} from 'react-router-dom'
 import DataProvider from './context/DataProvider'
 //components
 import About from './components/About'
+import Login from './components/Login'
+const PrivateRoute = ({ isAuthenticated, ...props }) => {
+  const token = sessionStorage.getItem('accessToken');
+  return isAuthenticated && token ? 
+    <>
+      <Header />
+      <Outlet />
+    </> : <Navigate replace to='/account' />
+};
 
 function App() {
-  const [count, setCount] = useState(0)
 
+  const [isAuthenticated, isUserAuthenticated] = useState(false);
   return (
     <DataProvider>
       <BrowserRouter>
       <Box style={{ marginTop: 64}}>
         <Routes>
+          <Route  path='/account' element={<Login isUserAuthenticated={isUserAuthenticated} />}/>
+          <Route>
+
+          </Route>
           <Route>
             <Route path='/about' element={<About />} />
           </Route>
