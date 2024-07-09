e8f1e63b4594eb841cdc361972d2bc9a393f4e12
Habeeb <ademolawaliyu1@gmail.com>
2024-07-09 09:28:08 +0100

    completing the task

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,11 +5,17 @@ import DataProvider from './context/DataProvider'
 //components
 import About from './components/About'
 import Login from './components/Login'
+import Home from './components/home/Home'
+import CreatePost from './components/CreatePost'
+import Header from './components/Header'
+import Contact from './components/Contact'
+import Update from './components/Update'
+import DetailView from './components/details/DetailView'
 const PrivateRoute = ({ isAuthenticated, ...props }) => {
   const token = sessionStorage.getItem('accessToken');
   return isAuthenticated && token ? 
     <>
-      <Header />
+    
       <Outlet />
     </> : <Navigate replace to='/account' />
 };
@@ -21,14 +27,33 @@ function App() {
     <DataProvider>
       <BrowserRouter>
       <Box style={{ marginTop: 64}}>
+      <Header />
         <Routes>
-          <Route  path='/account' element={<Login isUserAuthenticated={isUserAuthenticated} />}/>
-          <Route>
+        <Route path='/account' element={<Login isUserAuthenticated={isUserAuthenticated} />} />
+            
+            {/* <Route path='/' element={<PrivateRoute isAuthenticated={isAuthenticated} />} > */}
+              <Route path='/' element={<Home />} />
+            {/* </Route> */}
 
-          </Route>
-          <Route>
-            <Route path='/about' element={<About />} />
-          </Route>
+            {/* <Route path='/create' element={<PrivateRoute isAuthenticated={isAuthenticated} />} > */}
+              <Route path='/create' element={<CreatePost />} />
+            {/* </Route> */}
+{/* 
+            <Route path='/details/:id' element={<PrivateRoute isAuthenticated={isAuthenticated} />} > */}
+              <Route path='/details/:id' element={<DetailView />} />
+            {/* </Route> */}
+
+            <Route path='/update/:id' element={<PrivateRoute isAuthenticated={isAuthenticated} />} >
+              <Route path='/update/:id' element={<Update />} />
+            </Route>
+
+            {/* <Route path='/about' element={<PrivateRoute isAuthenticated={isAuthenticated} />} > */}
+              <Route path='/about' element={<About />} />
+            {/* </Route> */}
+{/* 
+            <Route path='/contact' element={<PrivateRoute isAuthenticated={isAuthenticated} />} > */}
+              <Route path='/contact' element={<Contact />} />
+            {/* </Route> */}
         </Routes>
 
       </Box>
